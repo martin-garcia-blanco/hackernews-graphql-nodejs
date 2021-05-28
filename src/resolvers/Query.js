98fd4be9a589160d21ce45b576dd3f99d8439623
martin-garcia-blanco <programadopormartin@gmail.com>
2021-05-28 14:43:43 +0200

    Add filtering to feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,15 @@
-function feed(parent, args, context, info) {
-  return context.prisma.link.findMany();
+async function feed(parent, args, context, info) {
+  const { filter } = args;
+  const linkList = await context.prisma.link.findMany();
+
+  if (Boolean(filter)) {
+    const newList = linkList.filter(
+      (link) => link.url.includes(filter) || link.description.includes(filter)
+    );
+    return newList;
+  }
+
+  return linkList;
 }
 
 module.exports = {
